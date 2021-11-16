108d25310a29c7669de47beb32fca3254930a522
mritthika <hmritthika@gmail.com>
2021-11-15 16:42:14 -0800

    starting to add in sample data

---
+++ db_scripts/insert.sql
@@ -0,0 +1,20 @@
+CREATE TABLE movies (
+    title       text
+    year        varchar(4)
+    lead_actor  text
+);
+-- insert sample data
+
+CREATE TABLE franchise (
+    title           text
+    franchise_name  text
+);
+
+-- insert data 
+
+CREATE TABLE reviews (
+    star_rating     integer
+    comment         text
+);
+
+-- insert data 
