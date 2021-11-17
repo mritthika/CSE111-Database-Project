041a351d8656dde1690170561d472dc08124e70b
mritthika <hmritthika@gmail.com>
2021-11-16 17:54:33 -0800

    added in movie_key to database

--- db-functions.sql
+++ db-functions.sql
@@ -1,15 +1,63 @@
--- Insert to add in new movies 
+-- user make account
 
--- Delete Movie (for all the bad reviews) (admin feature)
+-- admin make account 
 
--- Creating appointments (user, admin)
+-- user login
 
--- Bookmarking movies (user)
+-- admin login 
 
--- Leaving reviews (user, admin)
+-- user search movie by franchise
 
--- 
+-- admin search movie by franchise
 
--- 
+-- user search movie by year
 
--- 
+-- admin search movie by year
+
+-- user search movie by title
+
+-- admin search movie by title
+
+-- user search movie by movie key
+
+-- admin search movie by movie
+
+-- user comments on movies
+
+-- admin comments on movies
+
+-- user scores movies /5
+
+-- admin scores movies /5
+
+-- user makes appt
+
+-- admin makes appt
+
+-- user cancels appt
+
+-- admin cancels appt
+
+-- user invites other users to appt
+
+-- admin invites other user to appt 
+
+-- user bookmarks movies by year
+
+-- admin bookmarks movies by year
+
+-- user bookmarks movies by movie key
+
+-- admin bookmarks movies by movie key
+
+-- user bookmarks movies by franchise
+
+-- admin bookmarks movies by franchise
+
+-- user bookmarks movies by title
+
+-- admin bookmarks movies by title
+
+-- admin adds movies
+
+-- admin deletes movies
